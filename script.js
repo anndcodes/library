f89bfedf71ee0d7bdaf0f313b9a4e0b4aa8dda4f
Annd <ssandreia30@gmail.com>
2023-09-12 19:30:16 -0300

    add books objects to array

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const addBook = document.getElementById("add-book");
 const modal = document.querySelector("dialog");
-const closeModal = document.querySelector("dialog #close-modal");
-const submitBook = document.querySelector("dialog #submit-book");
+const closeModal = document.querySelector("#close-modal");
+const submitBook = document.querySelector("#submit-book");
 let bookInfo = document.querySelector("dialog form");
 
 //variables that will hold book infos
@@ -32,3 +32,29 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
+
+//add books to array
+function addBookToLibrary() {
+  bookInfo.addEventListener("submit", function (e) {
+    e.preventDefault();
+    title = document.getElementById("title").value;
+    author = document.getElementById("author").value;
+    pages = document.getElementById("pages").value;
+    read = Array.from(document.querySelectorAll(".read"));
+
+    myLibrary.push(new Book(title, author, pages,read));
+
+    // get index of the books
+    books = Object.keys(myLibrary);
+    for(let i = 0; i < books.length; i++) {
+      index = books[i];
+    }
+    
+    modal.close();
+    bookInfo.reset();
+  })
+}
+
+
+
+addBookToLibrary()
